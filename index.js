0873c196a5878557795b1ea8abe955f5a9227e9e
Ahha0801 <128229173+Ahha0801@users.noreply.github.com>
2023-05-16 09:44:50 -0500

    fixed a bunch of undefined errors

--- index.js
+++ index.js
@@ -4,80 +4,68 @@
 
 import inquirer from "inquirer";
 import { writeFile } from "fs";
+import SVG from "./lib/svg.js";
 import Triangle from "./lib/triangle.js";
 import Circle from "./lib/circle.js";
 import Square from "./lib/square.js";
 
-const questions = [
-  {
-    type: "input",
-    name: "initials",
-    message: "What will the text for the logo be? (Max 3 characters)",
-  },
-  {
-    type: "input",
-    name: "textcolor",
-    message: "What should the color of the text be? Enter a color.",
-  },
-  {
-    type: "list",
-    name: "shape",
-    choices: ["circle", "square", "triangle"],
-    message: "Choose the shape of the logo be?",
-  },
-  {
-    type: "input",
-    name: "fillcolor",
-    message: "What should the shape's color be? Enter a color.",
-  },
-];
-
-function writeToFile(data) {
+const writeToFile = ({ text, textC, shape, shapeC }) => {
   let generatedShape;
 
-  switch (data.shape) {
+  switch (shape) {
     case "circle":
-      generatedShape = new Circle(
-        data.fillcolor,
-        data.initials,
-        data.textcolor
-      );
+      generatedShape = new Circle();
       break;
     case "triangle":
-      generatedShape = new Triangle(
-        data.fillcolor,
-        data.initials,
-        data.textcolor
-      );
+      generatedShape = new Triangle();
       break;
     case "square":
-      generatedShape = new Square(
-        data.fillcolor,
-        data.initials,
-        data.textcolor
-      );
+      generatedShape = new Square();
       break;
     default:
       console.error("Invalid shape entered!");
       return;
   }
 
-  generatedShape = generatedShape.render(
-    data.fillcolor,
-    data.initials,
-    data.textcolor
-  );
+  generatedShape.setColor(shapeC);
 
-  writeFile("examplesvg.svg", generatedShape, (err) =>
+  const svg = new SVG();
+
+  svg.setText(text, textC);
+  svg.setShape(generatedShape);
+  return writeFile("examplesvg.svg", svg.render(), (err) =>
     err ? console.error(err) : console.log("Logo generated!")
   );
-}
-
+};
 function init() {
-  inquirer.prompt(questions).then((data) => {
-    console.log(data);
-    writeToFile(data);
-  });
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "What will the text for the logo be? (Max 3 characters)",
+      },
+      {
+        type: "input",
+        name: "textC",
+        message: "What should the color of the text be? Enter a color.",
+      },
+      {
+        type: "list",
+        name: "shape",
+        choices: ["circle", "square", "triangle"],
+        message: "Choose the shape of the logo be?",
+      },
+      {
+        type: "input",
+        name: "shapeC",
+        message: "What should the shape's color be? Enter a color.",
+      },
+    ])
+
+    .then(({ text, textC, shape, shapeC }) => {
+      writeToFile({ text, textC, shape, shapeC });
+    });
 }
 
 init();
